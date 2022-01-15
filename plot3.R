14857b2f3903194c2e7530209eb74c603a899fd0
YaleFeng <42980368+YaleFeng@users.noreply.github.com>
2022-01-14 20:15:47 -0700

    update

--- plot3.R
+++ plot3.R
@@ -1,5 +1,6 @@
 # Load data
 library("data.table")
+library("ggplot2")
 path <- getwd()
 download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
               , destfile = paste(path, "dataFiles.zip", sep = "/"))
